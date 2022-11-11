9597d1064f66aa201c47f655e87dc32ca5e6fe74
StevenGinsberg <53059396+StevenGinsberg@users.noreply.github.com>
2022-11-11 14:20:39 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,162 +1,197 @@
 $(document).ready(function () {
-
-    $.ajax({
-        type: 'GET',
-        url: 'http://vending.us-east-1.elasticbeanstalk.com',
-        success: function (menuArray) {
-            // get a reference to the 'vending-menu' div
-            var vendingMenu = $('#vending-menu');
-
-            // go through each of the returned menu items and append the info to the
-            //vendingMenu
-            $.each(menuArray, function (index, item) {
-                var itemInfo = '<button class="menu-btn col-sm-4 col-md-4 col-lg-4" type="button" value="' + item.id + '">';
-                itemInfo += '<b>' + item.id + '</b>: ' + item.name;
-                itemInfo += '<br>';
-                itemInfo += '$' + parseFloat(Math.round(item.price * 100) / 100).toFixed(2);
-                itemInfo += '<br>';
-                itemInfo += '<br>';
-                itemInfo += 'Stock: ' + item.quantity;
-
-                vendingMenu.append(itemInfo);
-            })
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            alert("FAILURE!");
-        }
-    });
-
-    // $('.menu-btn').click(function () {
-    //     var itemId = $(this).val();
-    //     console.log(itemId);
-    //     $('#item-select').val(itemId);
-    // });
-
-    $('menu').on('click', '.menu-btn', function () {
-        var itemId = $(this).val();
-        console.log(itemId);
-        $('#item-select').val(itemId);
-    });
-
-    $('#purchase-btn').on('click', function () {
-        var amountVar = $('#balance').val();
-        var idVar = $('#item-select').val()
-        $.ajax({
-            type: 'POST',
-            url: 'http://tsg-vending.herokuapp.com/money/' + amountVar + '/item/' + idVar,
-            data: JSON.stringify({
-                amount: amountVar,
-                id: idVar
-            }),
-            success: function (coinArray) {
-                var message = 'Thank you for your purchase!';
-                document.getElementById('message-box').innerHTML = message;
-
-                var money = 0.00;
-                $('#balance').val(money);
-                document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-
-                var change = '';
-                if (coinArray.quarters > 0) {
-                    change += 'Quarters: ' + coinArray.quarters + '<br>';
-                }
-                if (coinArray.dimes > 0) {
-                    change += 'Dimes: ' + coinArray.dimes + '<br>';
-                }
-                if (coinArray.nickels > 0) {
-                    change += 'Nickels: ' + coinArray.nickels + '<br>';
-                }
-                if (coinArray.pennies > 0) {
-                    change += 'Pennies: ' + coinArray.pennies + '<br>';
-                }
-                if (change == '') {
-                    change += 'No change returned.'
-                }
-                document.getElementById('change-box').innerHTML = change;
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                var responseText = $.parseJSON(jqXHR.responseText);
-                var errorMessage = responseText.message;
-                document.getElementById('message-box').innerHTML = errorMessage;
-            }
-        });
-    });
-
-    $('#dollar-btn').click(function () {
-        var money = $('#balance').val();
-        money = +money + +1.00;
-        $('#balance').val(money);
-        document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-    });
-
-    $('#quarter-btn').click(function () {
-        var money = $('#balance').val();
-        money = +money + +0.25;
-        $('#balance').val(money);
-        document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-    });
-
-    $('#dime-btn').click(function () {
-        var money = $('#balance').val();
-        money = +money + +0.10;
-        $('#balance').val(money);
-        document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-    });
-
-    $('#nickel-btn').click(function () {
-        var money = $('#balance').val();
-        money = +money + +0.05;
-        $('#balance').val(money);
-        document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-    });
-
-    $('#change-btn').on('click', function () {
-        var money = $('#balance').val() * 100;
-        var quarters = 0;
-        var dimes = 0;
-        var nickels = 0;
-        var pennies = 0;
-
-        while (money > 24) {
-            quarters++;
-            money -= 25;
-        }
-
-        while (money > 9) {
-            dimes++;
-            money -= 10;
-        }
-
-        while (money > 4) {
-            nickels++;
-            money -= 5;
-        }
-
-        while (money > 0) {
-            pennies++;
-            money -= 1;
-        }
-
-        $('#balance').val(money);
-        document.getElementById('balance').innerHTML = parseFloat(Math.round(money * 100) / 100).toFixed(2);
-
-        var change = '';
-        if (quarters > 0) {
-            change += 'Quarters: ' + quarters + '<br>';
-        }
-        if (dimes > 0) {
-            change += 'Dimes: ' + dimes + '<br>';
-        }
-        if (nickels > 0) {
-            change += 'Nickels: ' + nickels + '<br>';
-        }
-        if (pennies > 0) {
-            change += 'Pennies: ' + pennies + '<br>';
-        }
-        if (change == '') {
-            change += 'No change returned.'
-        }
-        document.getElementById('change-box').innerHTML = change;
-    });
-
+  loadItems();
+  addMoney();
+  purchaseItem();
 });
+
+function loadItems(){
+  $.ajax({
+      type: 'GET',
+      url: 'http://vending.us-east-1.elasticbeanstalk.com/items',
+      success: function(itemArray) {
+        var itemsDiv = $('#allItems');
+        var count = 1;
+        $.each(itemArray, function(index, item) {
+            var itemInfo = '<div class="boxed'+count+'" onClick="selectItem('+item.id+')"><p ALIGN=Left>';
+            itemInfo += item.id +'</p><p ALIGN = Center>';
+            itemInfo += item.name + '<br><br>';
+            itemInfo += '$' + item.price + '<br><br>';
+            itemInfo += 'Quantity Left: ' + item.quantity + '<br>';
+            itemInfo += '</p></div>';
+
+            itemsDiv.append(itemInfo);
+            count++;
+        })
+      },
+      error: function() {
+          alert('FAILURE!');
+      }
+  })
+}
+
+function addMoney(){
+    $('#addDollar').on('click', function() {
+      $('#messages').val('');
+      $('#change').val('');
+      $('#totalMoneyIn').val(((parseFloat($('#totalMoneyIn').val()))+1.00).toFixed(2));
+    });
+
+    $('#addQuarter').on('click', function() {
+      $('#messages').val('');
+      $('#change').val('');
+      $('#totalMoneyIn').val(((parseFloat($('#totalMoneyIn').val()))+0.25).toFixed(2));
+    });
+
+    $('#addDime').on('click', function() {
+      $('#messages').val('');
+      $('#change').val('');
+      $('#totalMoneyIn').val(((parseFloat($('#totalMoneyIn').val()))+0.10).toFixed(2));
+    });
+
+    $('#addNickel').on('click', function() {
+      $('#messages').val('');
+      $('#change').val('');
+      $('#totalMoneyIn').val(((parseFloat($('#totalMoneyIn').val()))+0.05).toFixed(2));
+    });
+}
+
+function selectItem(id){
+  $('#item').val(id);
+  $('#messages').val('');
+  $('#change').val('');
+}
+
+function purchaseItem(){
+    $('#makePurchase').on('click', function() {
+      if ($('#item').val() == ''){$('#messages').val('Please make a selection.');}else{ //if no item selected
+
+      $.ajax({ //if item selected
+          type: 'POST',
+          url: 'http://vending.us-east-1.elasticbeanstalk.com/money/'+$('#totalMoneyIn').val()+'/item/'+$('#item').val()+'',
+          success: function(change) {
+           // clearItems();
+            //loadItems(); //refresh item stock
+            $('#item').val(''); //refresh item selection
+            $('#totalMoneyIn').val(0.00.toFixed(2)); //refresh money in
+            $('#messages').val('Thank You!!');
+
+            var quarters = change.quarters;
+            var dimes = change.dimes;
+            var nickels = change.nickels;
+            var pennies = change.pennies;
+
+            var changeMessage = '';
+
+            if (quarters == 1){
+              changeMessage+= '1 quarter';
+            }
+            else if (quarters>1){
+              changeMessage+= quarters+' quarters';
+            }
+
+            if (dimes == 1){
+              if (quarters>=1) {changeMessage+=', ';}
+              changeMessage+= '1 dime';
+            }
+            else if (dimes>1){
+              if (quarters>=1) {changeMessage+=', ';}
+              changeMessage+=dimes+' dimes';
+            }
+
+            if (nickels == 1){
+              if (quarters>=1 || dimes>=1) {changeMessage+=', ';}
+              changeMessage+= '1 nickel';
+            }
+            else if (nickels>1){
+              if (quarters>=1 || dimes>=1) {changeMessage+=', ';}
+              changeMessage+= nickels+' nickels';
+            }
+
+            if (pennies == 1){
+              if (quarters>=1 || dimes>=1 || nickels>=1) {changeMessage+=', ';}
+              changeMessage+= '1 penny';
+            }
+            else if (pennies>1){
+              if (quarters>=1 || dimes>=1 || nickels>=1) {changeMessage+=', ';}
+              changeMessage+=pennies+' pennies';
+            }
+
+            $('#change').val(changeMessage); //display change returned
+
+          },
+          error: function(error) { //if insufficient funds or sold out
+              $('#messages').val(error.responseText.slice(12,-2));
+              //clearItems();
+             // loadItems(); //refresh item stock
+          }
+        })
+      }
+    });
+}
+
+function clearItems(){
+  $('#allItems').remove();
+
+ var itemsDiv = $('.grid-container');
+  itemsDiv.append('<div id="allItems"></div>');
+}
+
+function returnChange(){
+  var money = $('#totalMoneyIn').val();
+  if (money==0){
+    $('#change').val('');
+  }
+  else{
+    var numQuarters = Math.floor(money/0.25);
+    money = (money % 0.25).toFixed(2);
+    var numDimes = Math.floor(money/0.1);
+    money = (money % 0.1).toFixed(2);
+    var numNickels = Math.floor(money/0.05);
+    money = (money % 0.05).toFixed(2);
+    var numPennies = money;
+
+    var changeMessage = '';
+
+    if (numQuarters == 1){
+      changeMessage+= '1 quarter';
+    }
+    else if (numQuarters>1){
+      changeMessage+= numQuarters+' quarters';
+    }
+
+    if (numDimes == 1){
+      if (numQuarters>=1) {changeMessage+=', ';}
+      changeMessage+= '1 dime';
+    }
+    else if (numDimes>1){
+      if (numQuarters>=1) {changeMessage+=', ';}
+      changeMessage+= numDimes+' dimes';
+    }
+
+    if (numNickels == 1){
+      if (numQuarters>=1 || numDimes>=1) {changeMessage+=', ';}
+      changeMessage+= '1 nickel';
+    }
+    else if (numNickels>1){
+      if (numQuarters>=1 || numDimes>=1) {changeMessage+=', ';}
+      changeMessage+= numNickels+' nickels';
+    }
+
+    if (numPennies == 1){
+      if (numQuarters>=1 || numDimes>=1 || numNickels>=1) {changeMessage+=', ';}
+      changeMessage+= '1 penny';
+    }
+    else if (numPennies>1){
+      if (numQuarters>=1 || numDimes>=1 || numNickels>=1) {changeMessage+=', ';}
+      changeMessage+=numPennies+' pennies';
+    }
+
+    $('#change').val(changeMessage); //display change returned
+    $('#totalMoneyIn').val(0.00.toFixed(2));
+
+  }
+  $('#messages').val('');
+  $('#item').val('');
+  clearItems();
+  loadItems(); //refresh item list
+}
